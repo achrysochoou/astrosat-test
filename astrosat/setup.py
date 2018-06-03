dd8b1300828b761a6888738d736291cb6c56507c
Allyn Treshansky <allyn.treshansky@gmail.com>
2018-06-03 20:21:14 +0100

    tweaking install script

--- astrosat/setup.py
+++ astrosat/setup.py
@@ -1,9 +1,6 @@
 import os
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), '../README.rst')) as readme:
-    README = readme.read()
-
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
@@ -48,7 +45,6 @@ setup(
     install_requires=REQUIREMENTS,
     license='MIT License', 
     description='Astrosat Django Test',
-    long_description=README,
     author='Allyn Treshansky',
     author_email="allyn.treshansky@gmail.com",
     entry_points={
